47406e104e567af7862e2538852bfe04e7bd07bc
iyokurot <01kayochin17neko@gmail.com>
2019-02-02 04:34:43 +0900

    2019/2/2

--- src/loadExcel.js
+++ src/loadExcel.js
@@ -1,25 +1,37 @@
 const { ipcRenderer, dialog } = require('electron').remote
 
 const selectDirBtn = document.getElementById('select-directory')
+const xlsx = require("xlsx")
 
 selectDirBtn.addEventListener('click', (event) => {
     //ipcRenderer.send('open-file-dialog')
     dialog.showOpenDialog({
-        properties: ['openFile', 'openDirectory']
+        properties: ['openFile'],
+        filters: [
+            { name: 'XLSX file', extensions: ['xlsx'] }
+        ]
     }, (files) => {
         if (files) {
-            console.log(files)
-            //event.sender.send('selected-directory', files)
+            //Excelファイルのパスを取得
             selectedFile(files)
+            var f = files[0]
+            let workfile = xlsx.readFile(f)
+            sheetNamePost(workfile.SheetNames)
 
         }
     })
 })
 
-ipcRenderer.on('selected-directory', (event, path) => {
-    document.getElementById('selected-file').innerHTML = `You selected: ${path}`
-})
 
 function selectedFile(path) {
     document.getElementById('selected-file').innerHTML = `You selected: ${path}`
+}
+
+function sheetNamePost(nameList) {
+    //シート名を送信
+    var sheetnames = ''
+    for (n in nameList) {
+        sheetnames += "<li>" + nameList[n] + "</li>"
+    }
+    document.getElementById('sheet-name').innerHTML = sheetnames
 }
